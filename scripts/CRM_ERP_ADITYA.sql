5e82c8cc9dff7fddec1699d714f2136672d97e9b
adityakgvv <adityavenkat353@gmail.com>
2026-07-24 11:32:29 +0530

    Add TRUNCATE statement before BULK INSERT

--- scripts/CRM_ERP_ADITYA.sql
+++ scripts/CRM_ERP_ADITYA.sql
@@ -52,7 +52,7 @@ CREATE TABLE bronze.erp_px_cat_g1v2(
 	maintenance NVARCHAR(50)
 
 );
-
+TRUNCATE TABLE bronze.crm_cust_info;
 BULK INSERT  bronze.crm_cust_info
 FROM 'D:\SQL with Baara\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH(
